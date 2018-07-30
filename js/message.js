67159e1e3ef17a1697b3aa1692098baf70b9fc02
chauncey <chchuang@foxmail.com>
2018-07-30 14:37:42 +0800

    mvc

--- js/message.js
+++ js/message.js
@@ -1,45 +1,45 @@
 
-		var APP_ID = 'kG67ohtbEjpk6ditRyjTzQP9-gzGzoHsz';
-		var APP_KEY = '1jV1YrVw4IoLvL3ltRgzShPt';
+var APP_ID = 'kG67ohtbEjpk6ditRyjTzQP9-gzGzoHsz';
+var APP_KEY = '1jV1YrVw4IoLvL3ltRgzShPt';
 
-		AV.init({
-			appId: APP_ID,
-			appKey: APP_KEY
-		});
+AV.init({
+	appId: APP_ID,
+	appKey: APP_KEY
+});
 
 
-		let myForm = document.querySelector('#postMessageForm')
-		myForm.addEventListener('submit', function (e) {
-			e.preventDefault()
-			let content = myForm.querySelector('input[name=content]').value
-			let name = myForm.querySelector('input[name=name]').value
-			var Message = AV.Object.extend('Message');
-			var message = new Message();
-			message.save({
-				'name': name,
-				'content': content
-			}).then(function (object) {
+let myForm = document.querySelector('#postMessageForm')
+myForm.addEventListener('submit', function (e) {
+	e.preventDefault()
+	let content = myForm.querySelector('input[name=content]').value
+	let name = myForm.querySelector('input[name=name]').value
+	var Message = AV.Object.extend('Message');
+	var message = new Message();
+	message.save({
+		'name': name,
+		'content': content
+	}).then(function (object) {
+		let li = document.createElement('li')
+		li.innerText = `${object.attributes.name}: ${object.attributes.content}`
+		let messageList = document.querySelector('#messageList')
+		messageList.appendChild(li)
+		myForm.querySelector('input[name=content]').value = ''
+		console.log(object)
+	})
+})
+
+
+
+var query = new AV.Query('Message');
+query.find()
+	.then(
+		function (messages) {
+			let array = messages.map((item) => item.attributes)
+			array.forEach((item) => {
 				let li = document.createElement('li')
-				li.innerText = `${object.attributes.name}: ${object.attributes.content}`
+				li.innerText = `${item.name}: ${item.content}`
 				let messageList = document.querySelector('#messageList')
 				messageList.appendChild(li)
-				myForm.querySelector('input[name=content]').value = ''
-				console.log(object)
 			})
-		})
-
-
-
-		var query = new AV.Query('Message');
-		query.find()
-			.then(
-				function (messages) {
-					let array = messages.map((item) => item.attributes)
-					array.forEach((item) => {
-						let li = document.createElement('li')
-						li.innerText = `${item.name}: ${item.content}`
-						let messageList = document.querySelector('#messageList')
-						messageList.appendChild(li)
-					})
-				}
-			)
+		}
+	)
--- js/swiper.js
+++ js/swiper.js
@@ -1,16 +1,33 @@
-var mySwiper = new Swiper('.swiper-container', {
-    autoplay: true,
-    loop: true,
-    preventClicks: true,
-    // 如果需要分页器
-    pagination: {
-        el: '.swiper-pagination',
-    },
+!function () {
+    var view = document.querySelector('#production')
+    var controller = {
+        view: null,
+        swiper: null,
+        swiperOptions: {
+            autoplay: true,
+            loop: true,
+            preventClicks: true,
+            // 如果需要分页器
+            pagination: {
+                el: '.swiper-pagination',
+            },
 
-    // 如果需要前进后退按钮
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
+            // 如果需要前进后退按钮
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+        },
+        init: function (view) {
+            this.view = view
+            this.initSwiper()
+        },
+        initSwiper: function () {
+            this.swiper = new Swiper(this.view.querySelector('.swiper-container'),
+                this.swiperOptions
+            )
+        }
+    }
+    controller.init(view)
 
-})   
+}.call()
